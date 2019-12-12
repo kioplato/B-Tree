bf7e60ead41b8f3b12e608c38ff62890759ed656
Plato <otalpster@gmail.com>
2019-12-12 16:23:47 +0200

    BL: split to header and source files

--- src/block/BL.c
+++ src/block/BL.c
@@ -1,33 +1,14 @@
 /*******************************************************************************
- * File: MT.h
- * Purpose: API for creating and interacting with blocks.
+ * File: BL.c
+ * Purpose: Implementation of API for creating and interacting with blocks.
 *******************************************************************************/
 
-#ifndef BL_H
-#define BL_H
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#include "BF.h"
-#include "AM.h"
+#include "BL.h"
+#include "../BF.h"
+#include "../accessmethod/AM.h"
 
-#define CALL_BL(func_call)	\
-{							\
-	int code = func_call;	\
-	if (code != AME_OK)		\
-		AM_errno = code;	\
-		return code;		\
-}
-
-/*
- * Create a new block and allocate it.
- *
- * Returns AME_OK on success and sets block to new block.
- * Returns AME_ERROR on failure.
- */
-// TODO: return block id as well.
 int BL_CreateBlock(int file_desc, BF_Block** block)
 {
 	*block = NULL;
@@ -49,5 +30,3 @@ int BL_LoadBlock(int file_desc, int block_id, BF_Block** block)
 
 	return AME_OK;
 }
-
-#endif  // #ifndef BL_H
---
+++ src/block/BL.h
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * File: BL.h
+ * Purpose: API for creating and interacting with blocks.
+*******************************************************************************/
+
+#ifndef BL_H
+#define BL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../BF.h"
+#include "../accessmethod/AM.h"
+
+#define CALL_BL(func_call)	\
+{							\
+	int code = func_call;	\
+	if (code != AME_OK)		\
+		AM_errno = code;	\
+		return code;		\
+}
+
+/*
+ * Create a new block and allocate it.
+ *
+ * Returns AME_OK on success and sets block to new block.
+ * Returns AME_ERROR on failure.
+ */
+// TODO: return block id as well.
+int BL_CreateBlock(int file_desc, BF_Block** block);
+
+int BL_LoadBlock(int file_desc, int block_id, BF_Block** block);
+
+#endif  // #ifndef BL_H
