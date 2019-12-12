a71c9c44141b8e8684f5bbd4aab6fdc0a92c8d52
Plato <otalpster@gmail.com>
2019-12-12 14:31:56 +0200

    DB.c: reorder implementations to match DB.h

--- src/datablock/DB.c
+++ src/datablock/DB.c
@@ -12,48 +12,6 @@
 #include "../defn.h"
 #include "DB.h"
 
-int DB_Is_DataBlock(BF_Block* block, int* flag)
-{
-	char* data;
-	char datacode[] = DATACODE;  // The code for Data Blocks.
-
-	if (block == NULL) return AME_ERROR;
-
-	data = NULL;
-	data = BF_Block_GetData(block);
-	if (data == NULL) return AME_ERROR;
-
-	if (memcmp((const void*)data, (const void*)datacode, strlen(datacode)) == 0)
-		*flag = 1;
-	else
-		*flag = 0;
-
-	return AME_OK;
-}
-
-int DB_Get_MaxEntries(int file_desc, size_t* n_entries)
-{
-	char datacode[] = DATACODE;  // Used to calculate records per block.
-	size_t metadata_size;    // The metadata size of a Data Block.
-	size_t record_max_size;
-	size_t lenA;
-	size_t lenB;
-
-	if (n_entries == NULL) return AME_ERROR;
-
-	/* Metadata contains the data code, current block entries and next block. */
-	metadata_size = strlen(datacode) + sizeof(size_t) + sizeof(size_t);
-
-	CALL_FD(FD_Get_attrLength1(file_desc, &lenA));
-	CALL_FD(FD_Get_attrLength2(file_desc, &lenB));
-	record_max_size = lenA + lenB;
-
-	/* Calculate max entries. */
-	*n_entries = (BF_BLOCK_SIZE - metadata_size) / record_max_size;
-
-	return AME_OK;
-}
-
 int DB_Init(BF_Block* block, size_t next_block)
 {
 	char* data = NULL;  // The block's data.
@@ -73,6 +31,25 @@ int DB_Init(BF_Block* block, size_t next_block)
 	memcpy((void*)offseted_data, (const void*)&c_entries, sizeof(c_entries));
 	offseted_data += sizeof(c_entries);
 	memcpy((void*)offseted_data, (const void*)&next_block, sizeof(next_block));
+
+	return AME_OK;
+}
+
+int DB_Is_DataBlock(BF_Block* block, int* flag)
+{
+	char* data;
+	char datacode[] = DATACODE;  // The code for Data Blocks.
+
+	if (block == NULL) return AME_ERROR;
+
+	data = NULL;
+	data = BF_Block_GetData(block);
+	if (data == NULL) return AME_ERROR;
+
+	if (memcmp((const void*)data, (const void*)datacode, strlen(datacode)) == 0)
+		*flag = 1;
+	else
+		*flag = 0;
 
 	return AME_OK;
 }
@@ -118,71 +95,78 @@ int DB_Get_Entries(BF_Block* block, size_t* c_entries)
 	return AME_OK;
 }
 
-int DB_Shift_Records_Right(int file_desc, BF_Block* block, size_t shift_base, int* flag)
+int DB_Get_MaxEntries(int file_desc, size_t* n_entries)
 {
-	size_t c_entries;  // The number of entries the current block has.
-	size_t n_entries;  // The maximum number of entries the current block has.
+	char datacode[] = DATACODE;  // Used to calculate records per block.
+	size_t metadata_size;    // The metadata size of a Data Block.
+	size_t record_max_size;
+	size_t lenA;
+	size_t lenB;
 
-	size_t record_size;  // The maximum size of a record.
-	size_t lengthA;  // The length of the first field.
-	size_t lengthB;  // The length of the second field.
+	if (n_entries == NULL) return AME_ERROR;
 
-	char* data;
-	char* offseted_data;
-	char datacode[] = DATACODE;
+	/* Metadata contains the data code, current block entries and next block. */
+	metadata_size = strlen(datacode) + sizeof(size_t) + sizeof(size_t);
 
-	size_t move_bytes;
+	CALL_FD(FD_Get_attrLength1(file_desc, &lenA));
+	CALL_FD(FD_Get_attrLength2(file_desc, &lenB));
+	record_max_size = lenA + lenB;
+
+	/* Calculate max entries. */
+	*n_entries = (BF_BLOCK_SIZE - metadata_size) / record_max_size;
+
+	return AME_OK;
+}
+
+int DB_Get_Record(int file_desc, BF_Block* block, Record* record, size_t c_entry, int* flag)
+{
+	size_t n_entries;  // The maximum number of entries in a Data Block.
+	size_t c_entries;  // The current amount of entries in the Data Block.
+	size_t record_max_size;
+	size_t lenA;
+	size_t lenB;
+
+	char* data;  // The block's data.
+	char* offseted_data;  // Block's data for traversing contents.
 
 	if (block == NULL) return AME_ERROR;
+	if (record == NULL) return AME_ERROR;
 	if (flag == NULL) return AME_ERROR;
 
-	/* We guarantee the Data Block is not full. */
-	CALL_DB(DB_Get_Entries(block, &n_entries));
-	if (shift_base >= n_entries) {
-		*flag = 0;
+	CALL_DB(DB_Get_MaxEntries(file_desc, &n_entries));
+
+	if (c_entry >= n_entries) {
+		*flag = -1;
 		return AME_OK;
 	}
 
 	CALL_DB(DB_Get_Entries(block, &c_entries));
-	if (shift_base >= c_entries) {
+
+	if (c_entry >= c_entries) {
 		*flag = 0;
 		return AME_OK;
 	}
 
-	CALL_FD(FD_Get_attrLength1(file_desc, &lengthA));
-	CALL_FD(FD_Get_attrLength2(file_desc, &lengthB));
-	record_size = lengthA + lengthB;
+	CALL_FD(FD_Get_attrLength1(file_desc, &lenA));
+	CALL_FD(FD_Get_attrLength2(file_desc, &lenB));
+	record_max_size = lenA + lenB;
 
 	data = NULL;
 	data = BF_Block_GetData(block);
 	if (data == NULL) return AME_ERROR;
 
 	offseted_data = data;
-	offseted_data += strlen(datacode);
-	offseted_data += sizeof(size_t);  // Skip c_entries.
-	offseted_data += sizeof(size_t);  // Skip next_block.
-	offseted_data += shift_base * record_size;
+	offseted_data += strlen(DATACODE);
+	offseted_data += sizeof(size_t);
+	offseted_data += sizeof(size_t);
 
-	move_bytes = (shift_base - c_entries) * record_size;
-
-	/*
-	 * Could have implemented this with a reverse iteration of the records.
-	 * Get_Record and then write it after record_size bytes.
-	 * I chose to implement it using memmove.
-	 * Probably the Get_Record would have been cleaner.
-	 */
-	memmove((void*)(offseted_data + record_size), (void*)offseted_data, move_bytes);
+	offseted_data += record_max_size * c_entry;
+	
+	memcpy((void*)record->fieldA, (const void*)offseted_data, record->lengthA);
+	offseted_data += record->lengthA;
+	memcpy((void*)record->fieldB, (const void*)offseted_data, record->lengthB);
 
 	*flag = 1;
-
-	return AME_OK;
-}
-
-int DB_Print(int file_desc, BF_Block* block)
-{
-	
-
-	if (block == NULL) return AME_ERROR;
 
 	return AME_OK;
 }
@@ -272,53 +256,60 @@ int DB_Write_Record(int file_desc, BF_Block* block, Record record, size_t c_entr
 	return AME_OK;
 }
 
-int DB_Get_Record(int file_desc, BF_Block* block, Record* record, size_t c_entry, int* flag)
+int DB_Shift_Records_Right(int file_desc, BF_Block* block, size_t shift_base, int* flag)
 {
-	size_t n_entries;  // The maximum number of entries in a Data Block.
-	size_t c_entries;  // The current amount of entries in the Data Block.
-	size_t record_max_size;
-	size_t lenA;
-	size_t lenB;
+	size_t c_entries;  // The number of entries the current block has.
+	size_t n_entries;  // The maximum number of entries the current block has.
 
-	char* data;  // The block's data.
-	char* offseted_data;  // Block's data for traversing contents.
+	size_t record_size;  // The maximum size of a record.
+	size_t lengthA;  // The length of the first field.
+	size_t lengthB;  // The length of the second field.
+
+	char* data;
+	char* offseted_data;
+	char datacode[] = DATACODE;
+
+	size_t move_bytes;
 
 	if (block == NULL) return AME_ERROR;
-	if (record == NULL) return AME_ERROR;
 	if (flag == NULL) return AME_ERROR;
 
-	CALL_DB(DB_Get_MaxEntries(file_desc, &n_entries));
-
-	if (c_entry >= n_entries) {
-		*flag = -1;
-		return AME_OK;
-	}
-
-	CALL_DB(DB_Get_Entries(block, &c_entries));
-
-	if (c_entry >= c_entries) {
+	/* We guarantee the Data Block is not full. */
+	CALL_DB(DB_Get_Entries(block, &n_entries));
+	if (shift_base >= n_entries) {
 		*flag = 0;
 		return AME_OK;
 	}
 
-	CALL_FD(FD_Get_attrLength1(file_desc, &lenA));
-	CALL_FD(FD_Get_attrLength2(file_desc, &lenB));
-	record_max_size = lenA + lenB;
+	CALL_DB(DB_Get_Entries(block, &c_entries));
+	if (shift_base >= c_entries) {
+		*flag = 0;
+		return AME_OK;
+	}
+
+	CALL_FD(FD_Get_attrLength1(file_desc, &lengthA));
+	CALL_FD(FD_Get_attrLength2(file_desc, &lengthB));
+	record_size = lengthA + lengthB;
 
 	data = NULL;
 	data = BF_Block_GetData(block);
 	if (data == NULL) return AME_ERROR;
 
 	offseted_data = data;
-	offseted_data += strlen(DATACODE);
-	offseted_data += sizeof(size_t);
-	offseted_data += sizeof(size_t);
+	offseted_data += strlen(datacode);
+	offseted_data += sizeof(size_t);  // Skip c_entries.
+	offseted_data += sizeof(size_t);  // Skip next_block.
+	offseted_data += shift_base * record_size;
 
-	offseted_data += record_max_size * c_entry;
-	
-	memcpy((void*)record->fieldA, (const void*)offseted_data, record->lengthA);
-	offseted_data += record->lengthA;
-	memcpy((void*)record->fieldB, (const void*)offseted_data, record->lengthB);
+	move_bytes = (shift_base - c_entries) * record_size;
+
+	/*
+	 * Could have implemented this with a reverse iteration of the records.
+	 * Get_Record and then write it after record_size bytes.
+	 * I chose to implement it using memmove.
+	 * Probably the Get_Record would have been cleaner.
+	 */
+	memmove((void*)(offseted_data + record_size), (void*)offseted_data, move_bytes);
 
 	*flag = 1;
 
